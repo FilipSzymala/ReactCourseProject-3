f9299f0e1a9b644cc856be083d5f5cce53031342
Filip Szymala <slimymenyt@gmail.com>
2022-05-30 00:39:55 +0200

    Refactored meme state a little, added text to memes and styling

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,27 @@
 import React from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+    
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        imageSrc: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    const getMemeImage = (event) => {
+        event.preventDefault();
+        const memeArr = memesData.data.memes
+        const ranNum = Math.floor(Math.random()*memeArr.length)
+        const url = memeArr[ranNum].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageSrc: url
+        }))
+    }
+    
     return (
         <main>
             <form className="meme-form">
@@ -14,8 +35,13 @@ export default function Meme() {
                     placeholder="bottom text"
                     className="form--input" 
                  />
-                <button className="form--btn">Get a new meme image 🖼</button>
+                <button className="form--btn" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </form>
+            <div className="meme">
+                <img src={meme.imageSrc} className="meme--image" />
+                <h2 className="meme--text top">One does not simply</h2>
+                <h2 className="meme--text bottom">Walk into Mordor</h2>
+            </div>
         </main>
     )
 }
